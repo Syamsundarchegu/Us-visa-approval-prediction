6cc00cf073cbf00d77d510bc040af2492fba3ecb
Syamsundarchegu <chegusyam396@gmail.com>
2024-09-02 15:12:02 +0530

    some files are added

---
+++ us_visa/logger/__init__.py
@@ -0,0 +1,14 @@
+import logging
+import os
+
+
+from from_root import from_root
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+log_dir = 'logs'
+
+logs_path = os.path.join(from_root(),log_dir,LOG_FILE)
+os.makedirs(log_dir,exist_ok=True)
+logging.basicConfig(filename=logs_path, style='{',format="{asctime} - {levelname} - {message}", level=logging.INFO)
